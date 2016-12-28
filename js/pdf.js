299746311afcefdd70ba410c5ea238c846020c3d
jimin.zhong <jimin.zhong@tcl.com>
2016-12-28 18:36:02 +0800

    添加PDFjs在线预览测试

---
+++ js/pdf.js
@@ -0,0 +1,13 @@
+var umVersion="0.2"
+    var hash = window.location.hash;
+    var language = hash.split("#")[1];
+    var langArr=["en","fr","es","es-rES","es-rMX","es-419"];
+    if($.inArray(language, langArr) === -1) {
+      language = "en";
+    }
+
+    if(language=="es-rES"||language=="es-rMX"||language=="es-419"){
+      language="es";
+    }
+    window.location.replace("http://www.alcatel-move.com/tracker/help/pdfjs/web/viewer.html?file=GPS_Tracker_UM_"+language+".pdf?"+umVersion)
+    //window.location.replace("http://127.0.0.1:9096/pdfjs/web/viewer.html?file=GPS_Tracker_UM_"+language+".pdf?"+umVersion)
