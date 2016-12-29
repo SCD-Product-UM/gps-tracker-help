a9c2c63999063a1c5d48ae2d08c375a52167d7da
jimin.zhong <jimin.zhong@tcl.com>
2016-12-29 16:46:26 +0800

    区分Latam UM与其他通用UM。Latam UM照旧使用以前WebUI这边写的html，语言包括英语及Latam 西班牙语。新的通用UM用UM组提供的PDF转出的html，目前通用语言有英语、西班牙语、法语。

--- js/html.js
+++ js/html.js
@@ -1,14 +1,25 @@
-var umVersion="0.2"
-    var hash = window.location.hash;
-    var language = hash.split("#")[1];
-    var langArr=["en","fr","es","es-rES","es-rMX","es-419"];
-    if($.inArray(language, langArr) === -1) {
-      language = "en";
-    }
+var umVersion = "0.2"
+//var testUrl = "http://www.alcatel-move.com/tracker/help/index.html&version=latam#en"
+var href = window.location.href;
+var isLatamVersion = false;
+if (href.indexOf("version=latam") > -1) {
+  isLatamVersion = true;
+}
+var hash = window.location.hash;
+var language = hash.split("#")[1];
+var langArr = ["en", "fr", "es", "es-rES", "es-rMX", "es-419"];
+if ($.inArray(language, langArr) === -1) {
+  language = "en";
+}
 
-    if(language=="es-rES"||language=="es-rMX"||language=="es-419"){
-      language="es";
-    }
-    var domainUrl = "http://www.alcatel-move.com/tracker/help";
-    //var domainUrl = "http://127.0.0.1:9096";    
-    window.location.replace(domainUrl+"/html/"+language+"/"+language+".html?"+umVersion)
+if (language == "es-rES" || language == "es-rMX" || language == "es-419") {
+  language = "es";
+}
+var domainUrl = "http://www.alcatel-move.com/tracker/help";
+//var domainUrl = "http://127.0.0.1:9096";
+if(isLatamVersion){
+  window.location.replace(domainUrl + "/latam.html"+hash);  
+}else{
+  window.location.replace(domainUrl + "/html/" + language + "/" + language + ".html?" + umVersion);  
+}    
+
