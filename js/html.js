4175a2493bd93549af872f58ec43a934725b5e4f
jinhui.lu <jinhui.lu@tcl.com>
2017-02-24 13:51:59 +0800

    添加匈牙利语html

--- js/html.js
+++ js/html.js
@@ -7,7 +7,7 @@ if (href.indexOf("version=general") > -1) {
 }
 var hash = window.location.hash;
 var language = hash.split("#")[1];
-var langArr = ["en", "fr", "es", "es-rES", "es-rMX", "es-419"];
+var langArr = ["en", "fr", "es", "es-rES", "es-rMX", "es-419", "hu"];
 if ($.inArray(language, langArr) === -1) {
   language = "en";
 }
