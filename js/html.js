fd6986e4dd2d48068921129693ace174ba4e804d
jinhui.lu <jinhui.lu@tcl.com>
2017-04-19 19:29:00 +0800

    添加语言容错处理

--- js/html.js
+++ js/html.js
@@ -1,5 +1,7 @@
 var umVersion = "0.2"
+
 //var testUrl = "http://www.alcatel-move.com/tracker/help/index.html&version=latam#en"
+
 var href = window.location.href;
 var isLatamVersion = true;
 if (href.indexOf("version=general") > -1) {
@@ -7,10 +9,17 @@ if (href.indexOf("version=general") > -1) {
 }
 var hash = window.location.hash;
 var language = hash.split("#")[1];
-var langArr = ["en", "fr", "es", "es-rES", "es-rMX", "es-419", "hu", "ro", "sk","tr"];//通用版本
-var latamCurrentList = ["en", "es", "es-rES", "es-rMX", "es-419","en_US","en-US","en-us","en_us"];//latam版本
-if ($.inArray(language, isLatamVersion?latamCurrentList:langArr) === -1) {
-  language = "en";
+var langArr = ["en", "fr", "es", "es-rES", "es-rMX", "es-419", "hu", "ro", "sk", "tr"]; //通用版本
+var latamCurrentList = ["en", "es", "es-rES", "es-rMX", "es-419", "en_US", "en-US", "en-us", "en_us"]; //latam版本
+
+if ($.inArray(language, isLatamVersion ? latamCurrentList : langArr) == -1) {
+  if (language.indexOf("_") != -1) {
+    language = language.split("_")[0]
+  } else if (language.indexOf("-") != -1) {
+    language = language.split("-")[0]
+  }else{
+    language = "en";
+  }
 }
 
 if (language == "es-rES" || language == "es-rMX" || language == "es-419") {
@@ -18,13 +27,12 @@ if (language == "es-rES" || language == "es-rMX" || language == "es-419") {
 }
 var domainUrl = "http://www.alcatel-move.com/tracker/help";
 //var domainUrl = "http://127.0.0.1:9096";
-if(isLatamVersion){
-  if(language=="en_US"||language=="en-US"||language=="en-us"||language=="en_us"||language=="en"||language=="en-rUS"){
-    window.location.replace(domainUrl + "/html/en_US/en_US.html?" + umVersion); 
-  }else{
-    window.location.replace(domainUrl + "/latam.html"+hash); 
+if (isLatamVersion) {
+  if (language == "en_US" || language == "en-US" || language == "en-us" || language == "en_us" || language == "en" || language == "en-rUS") {
+    window.location.replace(domainUrl + "/html/en_US/en_US.html?" + umVersion);
+  } else {
+    window.location.replace(domainUrl + "/latam.html" + hash);
   }
-}else{
-  window.location.replace(domainUrl + "/html/" + language + "/" + language + ".html?" + umVersion);  
-}    
-
+} else {
+  window.location.replace(domainUrl + "/html/" + language + "/" + language + ".html?" + umVersion);
+}
